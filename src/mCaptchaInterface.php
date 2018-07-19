86a7b213095fed69a94735ed0390931a754da036
lyhiving <lyhiving@gmail.com>
2018-07-19 17:36:14 +0800

    初始化

---
+++ src/mCaptchaInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace lyhiving\mCaptcha;
+
+/**
+ * 验证码接口
+ * Interface mCaptchaInterface
+ * @package lyhiving\mCaptcha
+ */
+interface mCaptchaInterface
+{
+    /**
+     * 创建验证图片
+     * @return mixed
+     */
+    public function create();
+
+    /**
+     * 将验证码图片保存到指定路径
+     * @param string $filename 物理路径
+     * @param int $quality 清晰度
+     * @return mixed
+     */
+    public function save($filename, $quality);
+
+    /**
+     * 获取验证码图片
+     * @param int $quality 清晰度
+     * @return mixed
+     */
+    public function output($quality);
+
+    /**
+     * 获取验证码内容
+     * @return mixed
+     */
+    public function getText();
+}
